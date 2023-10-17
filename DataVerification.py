68bb5bfe69cd3ea557bc96ad12c154abe705178e
martkakar <136683683+martkakar@users.noreply.github.com>
2023-10-17 22:48:59 +0200

    Update DataVerification.py
    dodanie bloku try except na wypadek braku klucza oraz wartości None dla danych kluczy

--- DataVerification.py
+++ DataVerification.py
@@ -4,34 +4,43 @@ class DataVerification:
 
         @staticmethod
         def phone_verification(contact):
-            if 'phone' in contact:    #sprawdzenie czy w kontaktach znajduje się klucz phone
+            try:
                 phone_number = contact['phone']
+                    if phone_number is None:
+                        print("No phone in contact")
+                        return False
                 pattern = re.compile(r'^(\+\d{1,3}\s?)?(\d{3}-\d{3}-\d{3}|\d{9,12})$') #stworzenie wzorca nr tel opcja samych cyfr, z kierunkowym i z "-"
                 result = bool(pattern.match(phone_number)) # uzyskanie wyniku True albo False w zależności czy numer został prawidłowo wpisany
                 return result
-            else:
+            except KeyError:
                 print ("No phone in contact")
                 return False
              
         @staticmethod
         def email_verification(contact):
-            if 'email' in contact:
+            try:
                 email = contact['email']
+                if email is None:
+                    print("No email in contact")
+                    return False
                 pattern = re.compile(r'^[a-zA-Z0-9][a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[a-zA-Z][a-zA-Z]+$')
                 result = bool(pattern.match(email))
                 return result
-            else:
+            except KeyError:
                 print ("No email in contact")
                 return False
 
         @staticmethod
         def birth_verification(contact):
-            if 'birth_date' in contact:
+            try:
                 birth = contact['birth_date']
+                if birth is None:
+                    print("No birth_date in contact")
+                    return False
                 pattern = re.compile(r'^\d{4}-\d{2}-\d{2}$')
                 result = bool(pattern.match(birth))
                 return result
-            else:
+            except KeyError:
                 print ("No birth_date in contact")
                 return False
         
