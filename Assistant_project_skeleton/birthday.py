e37510ff7db31190806a6bba3aaff097947367cd
Ola <dabrowska.olga.90@gmail.com>
2023-10-17 17:44:12 +0200

    Add files via upload

---
+++ Assistant_project_skeleton/birthday.py
@@ -0,0 +1,8 @@
+# Paweł
+
+
+# Napisałam byle co, żeby main działał
+def cooming_birthday(birthday_list: list) -> list:  # listy słowników
+    n = input("Enter the number of days left until the birthday\n")
+    cooming_birthday_list = birthday_list
+    return cooming_birthday_list
