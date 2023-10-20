cf6f6560494b2956aabfaabd968ddddca82d510f
alicia1916 <alicja.barylski@gmail.com>
2023-10-20 19:04:05 +0200

    w setupie packages to ''Gotowy-szkielet-mozna...''

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='Application-Personal-Assistant',
-    version='1',
-    description='Address book and notebook',
-    url='https://github.com/alicia1916/Application-Personal-Assistant.git',
-    author='group_2',
-    author_email='alicja.barylski@gmail.com',
-    license='MIT',
-    packages=['application_personal_assistant'],
-    install_requires=['python-Levenshtein'],
-    entry_points={'console_scripts': ['assistant = application_personal_assistant.main:main']}
+    name="Application-Personal-Assistant",
+    version="1",
+    description="Address book and notebook",
+    url="https://github.com/alicia1916/Application-Personal-Assistant.git",
+    author="group_2",
+    author_email="alicja.barylski@gmail.com",
+    license="MIT",
+    packages=["Gotowy-szkielet-mozna-wklejac-swoje-kody"],
+    install_requires=["python-Levenshtein"],
+    entry_points={
+        "console_scripts": ["assistant = application_personal_assistant.main:main"]
+    },
 )
