daa9ab739f548a037fe3b04660a73065c582e635
martkakar <136683683+martkakar@users.noreply.github.com>
2023-10-22 15:40:06 +0200

    Update main.py - aktualizacja komunikatów wyświetlanych użytkownikowi

--- Gotowy_szkielet_mozna_wklejac_swoje_kody/main.py
+++ Gotowy_szkielet_mozna_wklejac_swoje_kody/main.py
@@ -1,12 +1,3 @@
-# Olka
-
-#
-# Opisy angielskie można śmiało poprawiać, gdyż mogę być językowo niepoprawne
-# Nazwy funkcji proponuję zostawić na razie takie jakie dałam, żeby działało, a potem ewentualnie zmieniać je podczas stand-upu, żeby nie było nieporozumienień
-#
-#
-# Imports - zostawiłam je na razie
-#
 from contacts import Contacts  # klasa Contacts
 from command_assistant import (
     command_assistant,
@@ -41,17 +32,17 @@ def check_and_call(fun, book: Contacts):
 def help_fun(book: Contacts):
     print(
         """List of keywords:
-        save - saving list of contacts to file - Pawel (save/load - inclass? outclass?)
-        add - add new contacts to your contacts book     (value/values required in next step/steps) - Marta (outer verification)
+        save - saving list of contacts to file
+        add - add new contacts to your contacts book
         show - display all information about contact/contacts    (value/values required in next step/steps)
         names - display all names with no more informations
-        notes - menage notes of choosen contact     (value/values required in next step/steps) - Kamil (outer notes manager)
-        birthday - display contacts whoose birthday are cooming... - Pawel (outer displayer)
+        notes - menage notes of choosen contact     (value/values required in next step/steps)
+        birthday - display contacts whoose birthday are cooming... 
         search - search contact by choosen field    (value/values required in next step/steps)
-        edit - edit contact     (value/values required in next step/steps) - Ala (editor - outclass? inclass?)
+        edit - edit contact     (value/values required in next step/steps)
         delete - remove contact     (value/values required in next step/steps)
         exit
-        if you write wrong command, assistant will help you - Pawel (outer command assistant)
+        if you write wrong command, assistant will help you
         """
     )
 
@@ -63,7 +54,7 @@ def help_fun(book: Contacts):
 
 def save_fun(book: Contacts):
     save_contacts(book.get_list_contacts())
-    print("save_fun done")  # ta linijka do usunięcia
+    #print("save_fun done")  # ta linijka do usunięcia
 
 
 #
@@ -84,7 +75,7 @@ def add_fun(book: Contacts):
 def show_fun(book: Contacts):
     while True:
         name = input(
-            "Enter the name of contact OR input all if you want to show all contacts OR input exit to resign\n"
+            "Enter the name of contact OR input 'all' if you want to show all contacts OR input 'exit' to resign\n"
         )
         if name.lower() == "all":
             # print(book.__str__())
@@ -136,7 +127,7 @@ def birthday_fun(book: Contacts):
 
 def edit_fun(book: Contacts):
     print(
-        "You need to enter the name of contact to edit name, address, phone, email, birthday of contact"
+        "You need to enter the name of contact to edit: name, address, phone, email, birthday of contact"
     )
     check_and_call(book.edit, book)
 
@@ -149,7 +140,7 @@ def edit_fun(book: Contacts):
 def search_fun(book: Contacts):
     while True:
         keyword = input(
-            "Write the name of field by that you want to search contacts (address/ phone/ email/ birthady/ notes) or input exit to resign\n"
+            "Write the name of field by that you want to search contacts (address/ phone/ email/ birthady/ notes) or input 'exit' to resign\n"
         )
         if keyword == "exit":
             break
@@ -194,7 +185,7 @@ KEYWORDS = {
 
 
 def main():
-    print("\n\nHello Idiot, I am your virtual assistant")
+    print("\n\nHello User, I am your virtual assistant")
     my_contacts_book = Contacts()  # odpala się konstruktor klasy
     loaded_contacts = read_contacts()
     my_contacts_book.set_list_contacts(loaded_contacts)
@@ -221,7 +212,7 @@ def main():
                 KEYWORDS[command](my_contacts_book)
             except:
                 print(
-                    "I don't understand what do you want (you are fucking idiot)...\n***** *** \nWrite 'help' to display available commands"
+                    "Sorry, I don't understand what do you want...\n***** *** \nWrite 'help' to display available commands"
                 )
 
     save_contacts(my_contacts_book.get_list_contacts())
